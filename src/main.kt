aab617cf9392d7468b5818bf7223af7767910830
stkau <stkautoru@mail.ru>
2019-11-15 13:19:02 +0700

    a

--- src/main.kt
+++ src/main.kt
@@ -1,46 +1,63 @@
+import java.util.PriorityQueue
+class Candidate() : Comparable<Candidate>{
+    var surname:String = read("enter surname")
+    var initials:String = read("enter initials")
+    var dateOfBirth:String = read("enter dateOfBirth")
+    var popularityIndex:Int
+    var variants = listOf<String>(
+        "Поддержан президентом",
+        "Поддержан опозиционной партией",
+        "оппозиционный кандидат, который снимет свою кандидатуру в пользу кандидата № 1",
+        "прочие")
 
-class Candidate{
-    var surname:String = readLine()?:"error"
-    var initials:String = readLine()?:"error"
-    var dateOfBirth:String = readLine()?:"error"
-    var popularityIndex:String
+    private fun read(promptString: String):String{
+        println(promptString)
+        return readLine()?:"error"
+    }
 
     init{
-        var variants = listOf<String>(
-            "Поддержан президентом",
-            "Поддержан опозиционной партией",
-            "оппозиционный кандидат, который снимет свою кандидатуру в пользу кандидата № 1",
-            "прочие")
-        variants.forEach { print(it) }
-        print("Выберите индекс популярности")
-        var ind :String = readLine()?:"no"
 
-        if(variants.indexOf(ind) != -1){
+        variants.forEachIndexed {index ,it  -> print("$index - $it \n") }
+        print("Выберите индекс популярности")
+        var ind :Int = readLine()?.toInt()?:-1
+
+        if(ind > -1 && ind < variants.size){
             popularityIndex = ind
         }else{
-            popularityIndex = "no"
+            popularityIndex = -1
+        }
+    }
+
+    override fun compareTo(b: Candidate): Int = when{
+        popularityIndex < b.popularityIndex -> -1
+        popularityIndex > b.popularityIndex -> 1
+        else -> 0
         }
 
-    }
+
 }
 
 class App{
-    private var candidatesList = ArrayList<Candidate>()
+    private var candidatesList = PriorityQueue<Candidate>()
 
 
-    fun add(canditate:Candidate){
-        candidatesList.add(canditate)
+    fun add(){
+        candidatesList.add(Candidate())
     }
 
-    fun remove(index:Int){
-        candidatesList.removeAt(index)
+    fun remove(){
+        candidatesList.poll()
     }
 
-    fun printList(){
-        candidatesList.forEach { print("Фамилия: ${it.surname} \n" +
-                "Инициалы: ${it.initials} \n" +
-                "Дата рождения: ${it.dateOfBirth} \n" +
-                "Индекс популярности ${it.popularityIndex}") }
+    fun printCandidates(){
+        var iterator = candidatesList.iterator()
+        while(iterator.hasNext()){
+            var it = iterator.next()
+            print("Фамилия: ${it.surname} \n" +
+                    "Инициалы: ${it.initials} \n" +
+                    "Дата рождения: ${it.dateOfBirth} \n" +
+                    "Индекс популярности ${it.popularityIndex} \n")
+        }
     }
 
 }
@@ -48,4 +65,19 @@ class App{
 
 fun main(){
 
+    var isWorking = true
+
+    var app = App()
+    var ways = listOf("1 - add new","2 - print All" , "3 - remove top")
+
+    while(isWorking){
+        ways.forEach { print("$it \n") }
+        var input = readLine()?.toInt()?:0
+        when(input){
+            0 -> print("incorect input \n")
+            1 -> app.add()
+            2 -> app.printCandidates()
+            3 -> app.remove()
+        }
+    }
 }
